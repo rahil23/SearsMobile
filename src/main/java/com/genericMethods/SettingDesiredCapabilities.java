cd7911204dd94d48ef722430a21c96a966057c63
rahil23 <info.rahil@gmail.com>
2017-04-16 13:37:34 +0530

    commiting apkfiles

--- src/main/java/com/genericMethods/SettingDesiredCapabilities.java
+++ src/main/java/com/genericMethods/SettingDesiredCapabilities.java
@@ -5,12 +5,13 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 
-import com.genericMethods.ReadPropertiesfile;
+
 
 public class SettingDesiredCapabilities extends AppiumServerStartStop {
 	public AndroidDriver driver;
@@ -27,13 +28,25 @@ public class SettingDesiredCapabilities extends AppiumServerStartStop {
 		cap.setCapability("platformVersion", ReadPropertiesfile.getConfigProperties("PLATFORMVERSION"));
 		cap.setCapability("platformName",ReadPropertiesfile.getConfigProperties("PLATFORMNAME"));
 		cap.setCapability("deviceName", ReadPropertiesfile.getConfigProperties("DEVICENAME"));
-cap.setCapability("appPackage", ReadPropertiesfile.getConfigProperties("APPPACKAGE"));
+cap.setCapability("app",".\\APKFiles\\Sears_10.3.apk" );
+		
+		cap.setCapability("appPackage", ReadPropertiesfile.getConfigProperties("APPPACKAGE"));
 cap.setCapability("appActivity", ReadPropertiesfile.getConfigProperties("APPACTIVITY"));
+cap.setCapability("unicodekeyboard", ReadPropertiesfile.getConfigProperties("UNICODEKEYBOARD"));
+cap.setCapability("resetkeyboard", ReadPropertiesfile.getConfigProperties("RESETKEYBOARD"));
 
 driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);  //TODO URL is method imported from "java.net" Package  and this will throw "MalformedURLException"
 
 driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
+driver.unlockDevice();
+//driver.resetApp();
 
+	}
+	
+	@AfterTest
+	public void closeApp()
+	{
+		driver.quit();
 	}
 }
